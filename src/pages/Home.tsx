ae377bf8f8829febe48d6c5ca6f54cc4d4133dc6
Greg Billetdeaux <gregadeaux@gmail.com>
2022-12-14 21:30:49 -0600

    Added ios and android support

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,7 +13,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Geolocation POC</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
